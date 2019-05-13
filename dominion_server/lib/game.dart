b08969c71978e47ad8b4c18978239b42ef048c75
Jen Thakar <jathak@gmail.com>
2019-05-12 18:05:45 -0700

    Add Seaside to server

--- dominion_server/lib/game.dart
+++ dominion_server/lib/game.dart
@@ -3,6 +3,8 @@ import 'package:dominion_core/dominion_core.dart';
 import 'package:dominion_sets/base_set.dart';
 // ignore: unused_import
 import 'package:dominion_sets/intrigue.dart';
+// ignore: unused_import
+import 'package:dominion_sets/seaside.dart';
 
 import 'dart:convert';
 import 'dart:io';
